f93ed13ff60ffaf174a588d9bbe4b26c534fcc32
Andrzej Gumula <agumula@corelogic.pl>
2021-07-29 13:25:31 +0200

    test comment

--- develapp-core/src/test/java/com/example/develapp/DevelappApplicationTests.java
+++ develapp-core/src/test/java/com/example/develapp/DevelappApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.develapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevelappApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
